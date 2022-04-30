190fbab11c75e14b2854c2e94f912c1682fd5fba
Phantom-Farmer <joeljdahl@gmail.com>
2022-04-30 13:09:41 -0500

    magick

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-console.log("shit be gettin REAL!");
+const hogHouses = ['G', 'H', 'R', 'S']
+
+const students = [
+  {
+    id: 1,
+    name: "Robby",
+    location: "Gryffindor",
+    enlisted: true
+  },
+
+  {
+    id: 2,
+    name: "T-Dog",
+    location: "Hufflepuff",
+    enlisted: true
+  },
+
+  {
+    id: 3,
+    name: "Eliza",
+    location: "Ravenclaw",
+    enlisted: true
+  },
+
+  {
+    id: 4,
+    name: "Hazel",
+    location: "Slytherin",
+    enlisted: false
+  },
+
+
+
+
+
+]
+
+
+const renderToDom = (divId, textToRender) => {
+  const selectedElement = document.querySelector(divId);
+  selectedElement.innerHTML = textToRender;
+};
