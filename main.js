e141fcce2d4c63b9b23f52df6f3ec10e8044c0c9
Phantom-Farmer <joeljdahl@gmail.com>
2022-04-30 13:32:12 -0500

    holy cow

--- main.js
+++ main.js
@@ -40,3 +40,24 @@ const renderToDom = (divId, textToRender) => {
   const selectedElement = document.querySelector(divId);
   selectedElement.innerHTML = textToRender;
 };
+
+const cardsOnDom = (pets) => {
+  let domString = "";
+  for (const member of pets) {
+    //const catType = member.type === "cat";
+    //const dogType = member.type === "dog";
+    //const dinoType = member.type === "dino";
+   domString += `<div class="card">
+   <div class="card-header">School of Magick</div>
+   <div class="card-body">
+     <h5 class="card-title">${student.name}</h5>
+     <p class="card-text">${student.location}</p>
+     <a href="#" class="btn btn-primary">EXPEL</a>
+   </div>
+ </div>`
+    
+ }
+ renderToDom("#app", domString);
+}
+
+cardsOnDom(students);
