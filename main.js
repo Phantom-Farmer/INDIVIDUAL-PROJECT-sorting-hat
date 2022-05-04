615cbab2d10b903094c4ca7aef2f7b0c2d46f8fd
Phantom-Farmer <joeljdahl@gmail.com>
2022-05-03 19:07:18 -0500

    i am so happy that i figured out some functioning event listeners

--- main.js
+++ main.js
@@ -2,31 +2,31 @@ const hogHouses = ['G', 'H', 'R', 'S']
 
 const students = [
   {
-    id: 1,
+    sId: 1,
     name: "Robby",
     location: "Gryffindor",
     enlisted: true
   },
 
   {
-    id: 2,
+    sId: 2,
     name: "T-Dog",
     location: "Hufflepuff",
     enlisted: true
   },
 
   {
-    id: 3,
+    sId: 3,
     name: "Eliza",
     location: "Ravenclaw",
     enlisted: true
   },
 
   {
-    id: 4,
+    sId: 4,
     name: "Hazel",
     location: "Slytherin",
-    enlisted: false
+    enlisted: true
   },
 
 ];
@@ -42,7 +42,7 @@ const renderToDom = (divId, textToRender) => {
 const sortOnDom = () => {
   let domString = "";
   domString =`
-  <div class="card" id="top" style="width: 18rem;">
+  <div class="card" id="top" style="width: 45rem;">
   <img src="assets/magick.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">MAGICK SCHOOL</h5> 
@@ -84,11 +84,11 @@ const cardsOnDom = (students) => {
     const ravenclawType = student.location === "Ravenclaw";
     const slytherinType = student.location === "Slytherin"
    domString += `<div class="card">
-   <div class="card-header">School of Magick</div>
+   <div class="card-header" id="head">School of Magick</div>
    <div class="card-body">
      <h5 class="card-title">${student.name}</h5>
      <p ${gryffindorType ? "class=gryffindor-type" : ""} ${hufflepuffType ? "class=hufflepuff-type" : ""} ${ravenclawType ? "class=ravenclaw-type" : ""} ${slytherinType ? "class=slytherin-type" : ""}>${student.location}</p>
-     <a href="#" class="btn btn-primary">EXPEL</a>
+     <a href="#" class="btn btn-primary" id="expel--${student.sId}">EXPEL</a>
    </div>
  </div>`
     
@@ -99,7 +99,7 @@ const cardsOnDom = (students) => {
 const expelledOnDom = () => {
    let domString = "";
    domString += `
-      <div class="card" id="lost" style="width: 18rem;">
+      <div class="card" id="lost" style="width: 45rem;">
         <img src="assets/fire.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h2>student name</h2>
@@ -112,7 +112,47 @@ const expelledOnDom = () => {
 
 }
 
+//*********************EVENT LISTENERS***************/
+
+const eventListeners = () => {
+
+//*********************FILTER BUTTON ROW************/
+
+document.querySelector("#filter-container").addEventListener("click", (e) => {
+  //console.log("you clicked a filter buttton", e.target.id); 
+  if (e.target.id === "all-btn") {
+    cardsOnDom(students);
+  } else if (e.target.id === "gryf-btn") {
+     const gryfs = students.filter(stud => stud.location === "Gryffindor");
+     cardsOnDom(gryfs);
+  } else if (e.target.id === "huff-btn") {
+    const huffs = students.filter(stud => stud.location === "Hufflepuff");
+    cardsOnDom(huffs);
+  } else if (e.target.id === "rav-btn") {
+    const ravs = students.filter(stud => stud.location === "Ravenclaw");
+    cardsOnDom(ravs);
+  } else if (e.target.id === "sly-btn") {
+    const slys = students.filter(stud => stud.location === "Slytherin");
+    cardsOnDom(slys);
+  } else if (e.target.id === "ex-btn") {
+    const exs = students.filter(stud => stud.location === "EXPELLED");
+    cardsOnDom(exs);
+  } 
+  
+ });
+}
+//*********************BUTTON on CARDS*************/
+
+document.querySelector("#student-container").addEventListener("click", (e) => {
+
+  if (e.target.id) {
+    
+  }
+
+}
+
 sortOnDom();
 buttonsOnDom();
 cardsOnDom(students);
 expelledOnDom();
+eventListeners()
